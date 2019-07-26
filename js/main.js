c674fb550dd70b33e022bafb9c922f42bc6c7a81
dayu <arizaqq@gmail.com>
2019-07-26 15:50:25 -0400

    checkbox

--- js/main.js
+++ js/main.js
@@ -3,9 +3,10 @@
 
   $(document).ready(function() {
     $.mobile.ajaxEnabled = false;
-    
+
     var input = $('#input');
     var output = $('#output');
+    var showpw = $('#checkbox-showpw');
 
     var execute = function() {
       try {
@@ -24,6 +25,16 @@
       output.prop('disabled', false);
       output.val(b64_sha512(input.val()).substring(0, 12))
       output.prop('disabled', true);
+    });
+
+    showpw.bind("change", function() {
+      if(showpw[0].checked) {
+        input.prop('type', 'password');
+        output.prop('type', 'password');
+      } else {
+        input.prop('type', 'input');
+        output.prop('type', 'input');
+      }
     });
 
     $('#execute').click(execute);
