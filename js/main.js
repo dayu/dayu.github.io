85dda03c433765fc4af65722bf7af578121492cc
dayu <arizaqq@gmail.com>
2019-07-26 16:17:11 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,12 +12,27 @@
       try {
 //        output.val(hex_sha512(input.val()).toUpperCase());
           output.prop('disabled', false);
+          if(!showpw[0].checked) {
+            output.prop('type', 'text');
+          }
           output.val(b64_sha512(input.val()).substring(0, 12)).select();
-          
+          if(!showpw[0].checked) {
+            output.prop('type', 'password');
+          }
           document.execCommand("copy");
           output.prop('disabled', true);
       } catch(e) {
         output.val(e);
+      }
+    }
+
+    function showpassword() {
+      if(showpw[0].checked) {
+        input.prop('type', 'text');
+        output.prop('type', 'text');
+      } else {
+        input.prop('type', 'password');
+        output.prop('type', 'password');
       }
     }
 
@@ -28,15 +43,10 @@
     });
 
     showpw.bind("change", function() {
-      if(showpw[0].checked) {
-        input.prop('type', 'text');
-        output.prop('type', 'text');
-      } else {
-        input.prop('type', 'password');
-        output.prop('type', 'password');
-      }
+      showpassword()
     });
 
+    showpassword();
     $('#execute').click(execute);
 
   });
