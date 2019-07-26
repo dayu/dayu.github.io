883e9523495dc30c4756b5ea9229e62636a6ea6d
dayu <arizaqq@gmail.com>
2019-07-25 22:43:41 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,9 +11,9 @@
     var execute = function() {
       try {
 //        output.val(hex_sha512(input.val()).toUpperCase());
-          output.val(b64_sha512(input.val()).substring(0, 12));
           output.prop('disabled', false);
-          output.val().select();
+          output.val(b64_sha512(input.val()).substring(0, 12)).select();
+          
           document.execCommand("copy");
           output.prop('disabled', true);
       } catch(e) {
